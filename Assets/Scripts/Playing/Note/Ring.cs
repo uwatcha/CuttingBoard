a7dcb6f80a42de006cfc2791c647e055c0e630dd
uwatcha <discoverywant30@gmail.com>
2024-12-16 14:13:09 +0900

    ノーツに縮むリングをつけた.

--- Assets/Scripts/Playing/Note/Ring.cs
+++ Assets/Scripts/Playing/Note/Ring.cs
@@ -1,6 +1,17 @@
 using UnityEngine;
 
-public class Ring
+public class Ring : MonoBehaviour
 {
-    
+    [SerializeField] float shrinkSpeed = 0.0005f;
+    [SerializeField] float minRingScale = 0.0445f;
+    [SerializeField] float justRingScale = 0.0445f;
+
+    private void Update()
+    {
+        Vector3 currentScale = transform.localScale;
+        if (currentScale.x > minRingScale)
+        {
+            transform.localScale = new(currentScale.x - shrinkSpeed, currentScale.y - shrinkSpeed, currentScale.z - shrinkSpeed);
+        }
+    }
 }
